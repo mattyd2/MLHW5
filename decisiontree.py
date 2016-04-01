8ec2b31e7f3aef649abb6d575052cb238a618640
Matthew Dunn <mattyd2@gmail.com>
2016-04-01 13:04:15 -0400

    adaboost almost complete

--- decisiontree.py
+++ decisiontree.py
@@ -7,16 +7,20 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 
 
-def decisiontree(depth):
+def decisiontree(depth, X, y):
+
+    # Train
+    clf = DecisionTreeClassifier(max_depth=depth)
+    clf.fit(X, y)
+    plotdecisiontree(X, y, clf, depth, 'TreeDepth')
+    return clf
+
+
+def plotdecisiontree(X, y, clf, depth, plttype):
     # Parameters
     n_classes = 2
     plot_colors = "bry"
     plot_step = 0.02
-
-    X, y = prepdata('train')
-
-    # Train
-    clf = DecisionTreeClassifier(max_depth=depth).fit(X, y)
 
     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
@@ -40,10 +44,9 @@ def decisiontree(depth):
     # plt.axis("tight")
     plt.suptitle("Decision surface of a decision tree using paired features")
     plt.legend()
-    plottype = "TreeDepth" + str(depth)
+    plottype = plttype + str(depth)
     plt.savefig(plottype)
     plt.close()
-    return clf
 
 
 # Question 2.2
@@ -59,6 +62,47 @@ def accuracyplot(classifiers, depths):
     plt.title(plottype)
     plt.ylabel('Error Rate')
     plt.xlabel('Depth of Tree')
+    plt.savefig(plottype)
+    plt.close()
+
+
+def decisiontreeAdaBoost(X, y, initialWeights):
+    err = 0
+    tot = 1
+    clfs = []
+    alphaMValues = []
+    for m in range(0, 10):
+        clf = DecisionTreeClassifier(max_depth=3)
+        print initialWeights
+        clf.fit(X, y, sample_weight=initialWeights)
+        # Plot the tree for this iteration
+        # plotdecisiontree(X, y, clf, 3, 'AdaBoost')
+        predictedValueM = clf.predict(X)
+        # Add the prediction to list
+        clfs.append(clf)
+        incorrectPredctMask = np.invert(np.equal(predictedValueM, y.ravel()))
+        W = np.copy(initialWeights)
+        errm = initialWeights[incorrectPredctMask].sum()/W.sum()
+        alpham = np.log((1.-errm)/errm)
+        # Add alpham value to list
+        alphaMValues.append(alpham)
+        newWeight = np.exp(alpham)
+        np.putmask(initialWeights, incorrectPredctMask, np.array(newWeight))
+    return accuracyplotAdaBoost(X, y, clfs, alphaMValues)
+
+
+# Question 3.3
+def accuracyplotAdaBoost(X, y, clfs, alphaMValues):
+    treeErrorRate = []
+    for m in range(0, 10):
+        y_predicted = clfs[m].predict(X)
+        treeErrorRate.append(1-accuracy_score(y, y_predicted))
+    plottype = "AdaBoost Performance on Test Data"
+    plt.plot(range(0, 10), treeErrorRate, label="Accuracy of AdaBoost on Test")
+    plt.legend()
+    plt.title(plottype)
+    plt.ylabel('Error Rate')
+    plt.xlabel('Number of Iterations (m)')
     plt.savefig(plottype)
     plt.close()
 
@@ -92,9 +136,8 @@ def loaddata(datatypetoload):
     # Load data
     fileDir = os.path.dirname(os.path.realpath('__file__'))
     if datatypetoload == 'train':
-        df = pd.read_csv(fileDir+"/datacopy/banana_train.csv", header=None)
+        df = pd.read_csv(fileDir+"/data/banana_train.csv", header=None)
     elif datatypetoload == 'test':
-        df = pd.read_csv(fileDir+"/datacopy/banana_test.csv", header=None)
-    dfprepped = df.replace(to_replace=-1, value=0)
-    dfvalues = dfprepped.values
+        df = pd.read_csv(fileDir+"/data/banana_test.csv", header=None)
+    dfvalues = df.values
     return dfvalues
--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import numpy as np
 import pandas as pd
 import os
-from decisiontree import decisiontree, accuracyplot
+import decisiontree as dt
 
 
 def main():
     classifiers = []
-    for i in range(1, 11):
-        decisiontreeclassifier = decisiontree(i)
-        classifiers.append(decisiontreeclassifier)
-    accuracyplot(classifiers, list(range(1, 11)))
+
+    # Question 2
+    X, y = dt.prepdata('train')
+    yNegOneOne = np.copy(y)
+    np.place(y, y == -1, 0)
+    # for depth in range(1, 11):
+    #     decisiontreeclassifier = dt.decisiontree(depth, X, y)
+    #     classifiers.append(decisiontreeclassifier)
+    # dt.accuracyplot(classifiers, list(range(1, 11)))
+
+    # Question 3
+    initialWeights = np.ones(yNegOneOne.shape[0])/yNegOneOne.shape[0]
+    dt.decisiontreeAdaBoost(X, yNegOneOne, initialWeights)
+    # dt.accuracyplot(classifiers, list(range(1, 11)))
 
 if __name__ == "__main__":
     main()
